b4ba5084846631b170c1a5122e742c050cb45264
Andrew Tolstov <awordes@yandex.ru>
2024-10-01 00:51:38 +0300

    Update appsetting

--- Ebote.API/Program.cs
+++ Ebote.API/Program.cs
@@ -34,15 +34,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
