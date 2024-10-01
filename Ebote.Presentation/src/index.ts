66ddd53fb101213d1b1e3b56f9743604e31a3a06
Andrew Tolstov <awordes@yandex.ru>
2024-10-02 00:12:01 +0300

    Add api client for presentation

--- Ebote.Presentation/src/index.ts
+++ Ebote.Presentation/src/index.ts
@@ -1,7 +1,12 @@
 import { ScreenLoader } from "./Application/Screens/ScreenLoader";
 import { MainScreen } from "./Application/Screens/Main/MainScreen";
+import { getAccountCheckAuth } from "./client";
 
 (async () => {
     await ScreenLoader.Init();
     await MainScreen.Init();
+    var result = await getAccountCheckAuth();
+
+    if (result.response.status != 200)
+        console.log("Unauthorized");
 })();
