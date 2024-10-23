18e6ec0fc02858b5386de54ec4b0fbdb125c5306
Andrew Tolstov <awordes@yandex.ru>
2024-10-23 15:16:56 +0300

    Add LoginForm

---
+++ Ebote.Presentation/src/ui/components/LoginForm.ts
@@ -0,0 +1,34 @@
+import { Container } from "pixi.js";
+import { InputField } from "./InputField";
+import { FancyButton } from "@pixi/ui";
+import { TextService } from "../../Localization/TextService";
+import { CreateButton } from "./Button";
+
+export class LoginForm extends Container {
+    public loginInput: InputField;
+    public passwordInput: InputField;
+    public loginButton: FancyButton;
+    public signupButton: FancyButton;
+
+    public static async Create(): Promise<LoginForm> {
+        let loginForm = new LoginForm();
+
+        loginForm.loginInput = await InputField.Create(TextService.GetStringValue('login'));
+
+        loginForm.passwordInput = await InputField.Create(TextService.GetStringValue('password'));
+        loginForm.passwordInput.position.set(0, loginForm.loginInput.height + 5);
+
+        loginForm.loginButton = await CreateButton(TextService.GetStringValue('signin'));
+        loginForm.loginButton.position.set(0, loginForm.passwordInput.y + loginForm.passwordInput.height + 5);
+
+        loginForm.signupButton = await CreateButton(TextService.GetStringValue('signup'));
+        loginForm.signupButton.position.set(0, loginForm.loginButton.y + loginForm.loginButton.height + 5);
+
+        loginForm.addChild(loginForm.loginButton);
+        loginForm.addChild(loginForm.signupButton);
+        loginForm.addChild(loginForm.passwordInput);
+        loginForm.addChild(loginForm.loginInput);
+
+        return loginForm;
+    }
+}
