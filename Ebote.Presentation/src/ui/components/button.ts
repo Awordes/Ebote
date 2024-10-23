788ef7891ddb37cbdbc5c2eb21a1eb59f3b3c657
Andrew Tolstov <awordes@yandex.ru>
2024-10-23 10:25:55 +0300

    Add button create function

---
+++ Ebote.Presentation/src/ui/components/button.ts
@@ -0,0 +1,23 @@
+import { FancyButton } from "@pixi/ui";
+import { Assets, Graphics, HTMLText } from "pixi.js";
+import { AssetStore } from "../../utils/assetStore";
+
+export async function CreateButton (text?: string): Promise<FancyButton> {
+    let button = new FancyButton({
+        text: new HTMLText({
+            text: text,
+            style: {
+                fontFamily: AssetStore.monocraftFont.alias,
+                fontSize: 100,
+            }
+        }),
+        padding: 3.9,
+        defaultView: new Graphics(await Assets.load(AssetStore.buttonDefault)),
+        pressedView: new Graphics(await Assets.load(AssetStore.buttonPressed)),
+        hoverView: new Graphics(await Assets.load(AssetStore.buttonHover)),
+        disabledView: new Graphics(await Assets.load(AssetStore.buttonDisabled)),
+        textOffset: { y: -1.5 },
+    });
+
+    return button;
+}
